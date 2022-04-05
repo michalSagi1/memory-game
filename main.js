5f44ace2e8c9b9368916dc1bb67bf51897030200
michal sagi <michaldinner@gmail.com>
2022-04-05 18:43:54 +0300

    card array in div

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("hi")
+const cards = ["A", "B", "C", "A", "B", "C"]
+const board = document.getElementById("board")
+
+
+ for (i of cards){
+    const element = document.createElement("div")
+    element.innerHTML = i
+    board.appendChild(element)
+
+ }
