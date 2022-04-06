d7542cb5bcb91c49eb70514bd6a5dc4fa8b24b90
michal sagi <michaldinner@gmail.com>
2022-04-07 00:43:23 +0300

    add mouse hover
    background color, font-bold, saman

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const cards = ["A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C"]
 function createCard(idx){
     const cardEl=document.createElement("div")
     cardEl.innerHTML=cards[idx];
-    cardEl.id=idx
+    cardEl.id=idx 
     cardEl.className="cards"
 
     return cardEl
@@ -18,10 +18,31 @@ shuffle(cards);
 
 
 const board = document.getElementById("board")
+
  for (i in cards){
     const element = createCard(i)
     board.appendChild(element)
+
  }
+
+ 
+ 
+//      document.getElementById("0").onmouseover = function() {mouseOver()};
+//      document.getElementById("0").onmouseout = function() {mouseOut()};
+
+    
+//  function mouseOver() {
+//     document.getElementById("0").style.backgroundColor= "rgb(77, 9, 9)";
+//   } 
+//   function mouseOut() {
+//   document.getElementById("0").style.backgroundColor = "rgb(227, 178, 199)"
+// }
+
+
+
+
+
+
 
 
 
